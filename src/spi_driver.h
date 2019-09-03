d3cb30545fff82b70907e437496ea4a076927831
Steven Daglish <s.c.daglish@gmail.com>
2019-09-03 13:30:46 +0100

    file details added

--- src/spi_driver.h
+++ src/spi_driver.h
@@ -1,3 +1,14 @@
+/**
+ * @file        spi_driver.h
+ * @author      Steven Daglish
+ * @brief       Mock file for spi_driver
+ * @version     0.1
+ * @date        03 September 2019
+ *
+ * @copyright Copyright (c) 2019
+ *
+ */
+
 #ifndef _SPI_DRIVER_H
 #define _SPI_DRIVER_H
 
